a97a14c07729454c91f9607f8700cb2206b9e52b
younginchoi9312 <78770261+younginchoi9312@users.noreply.github.com>
2021-02-25 17:42:50 +0900

    Add files via upload

---
+++ vowels2.py
@@ -0,0 +1,15 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+word = 'pyt'
+found = []
+
+for letter in word:
+    if letter in vowels:
+        if letter not in found:
+            found.append(letter)
+
+for vowel in found:
+    print(vowel)
+
+
+
---
+++ vowels3.py
@@ -0,0 +1,12 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+word = input("Provide a world to search for vowels: ")
+found = []
+
+for letter in word:
+    if letter in vowels:
+        if letter not in found:
+            found.append(letter)
+
+for vowel in found:
+    print(vowel)
